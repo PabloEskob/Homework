f08b0dcb1e3f927e29375cbcdba534d3d375c3e2
PabloEsk <pavelae27@mail.ru>
2023-06-23 21:29:48 +0300

    add button

---
+++ Assets/Homework/PresentationModel/Scripts/Buttons/LevelUpButton.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Lessons.Architecture.PM.Buttons
+{
+    public sealed class LevelUpButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [Space] [SerializeField] private Image _buttonBackground;
+        [SerializeField] private Sprite _availableButtonSprite;
+        [SerializeField] private Sprite _lockedButtonSprite;
+        [Space] [SerializeField] private TextMeshProUGUI _text;
+        [Space] [SerializeField] private State _state;
+        
+        public void AddListener(UnityAction action)
+        {
+            _button.onClick.AddListener(action);
+        }
+        public void RemoveListener(UnityAction action)
+        {
+            _button.onClick.RemoveListener(action);
+        }
+        
+        public void SetAvailable(bool isAvailable)
+        {
+            var state = isAvailable ? State.Available : State.Locked;
+            SetState(state);
+        }
+
+        public void SetState(State state)
+        {
+            _state = state;
+
+            if (state == State.Available)
+            {
+                _button.interactable = true;
+                _buttonBackground.sprite = _availableButtonSprite;
+            }
+            else if (state == State.Locked)
+            {
+                _button.interactable = false;
+                _buttonBackground.sprite = _lockedButtonSprite;
+            }
+            else
+            {
+                throw new Exception($"Undefined button state {state}!");
+            }
+        }
+
+
+#if UNITY_EDITOR
+
+        private void OnValidate()
+        {
+            try
+            {
+                SetState(_state);
+            }
+            catch (Exception)
+            {
+                Debug.Log("Exception");
+            }
+        }
+
+#endif
+    }
+}
+
+public enum State
+{
+    Available = 0,
+    Locked = 1
+}
--- Assets/Homework/PresentationModel/Scripts/Popups/CharacterPopup.cs
+++ Assets/Homework/PresentationModel/Scripts/Popups/CharacterPopup.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using Lessons.Architecture.PM.Buttons;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Lessons.Architecture.PM.Popup
 {
     public sealed class CharacterPopup : Popup
     {
-       
+        [SerializeField] private LevelUpButton _levelUpButton;
+        [SerializeField] private Button _closeButton;
+
+        protected override void OnShow(object args)
+        {
+            
+        }
+
+        protected override void OnHide()
+        {
+            
+        }
     }
 }
